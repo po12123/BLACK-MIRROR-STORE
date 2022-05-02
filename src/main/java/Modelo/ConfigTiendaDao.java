7b4feb37fa9d88cc75edfdd4d3c73b59f893722b
NoeGitHub <nj.espinoza714@gmail.com>
2022-05-02 19:43:52 -0400

    ConfiguracionTiendaDao

---
+++ src/main/java/Modelo/ConfigTiendaDao.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author njesp
+ */
+public class ConfigTiendaDao {
+    Conexion cn = new Conexion();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    
+    public boolean ActualizarTienda(ConfigTienda ctl){
+       String sql = "UPDATE datostienda SET NombreTienda=?,Nit=? , TelefonoT=?, DireccionT=?,RazonSocial=?  WHERE id=?";
+       try {
+           ps = con.prepareStatement(sql);   
+           ps.setString(1, ct1.getNombreT());
+           ps.setString(2, ct1.getNit());
+           ps.setString(3, ct1.getTelefonoT());
+           ps.setString(4, ct1.getDireccionT());
+           ps.setString(5, ct1.getMensaje());
+           ps.execute();
+           return true;
+       } catch (SQLException e) {
+           System.out.println(e.toString());
+           return false;
+       }finally{
+           try {
+               con.close();
+           } catch (SQLException e) {
+               System.out.println(e.toString());
+           }
+       }
+   }
+}
